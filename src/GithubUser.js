ad90078760980db1427612393661c7af0088d96f
Alex <bakerab@sbcglobal.net>
2017-05-30 16:15:38 -0400

    Add user to state

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component {
+    state = {
+        user: {
+            avatar_url: '',
+            login: '',
+            followers: '',
+            following: '',
+            location: '',
+            html_url: '',
+        }
+    }
+    constructor(props){
+        super(props)
+        this.fetchUserData()
+    }
+
+    fetchUserData = () => {
+        fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+        .then(response => response.json())
+        .then(user => console.log(user))
+    }
+
     render(){
-        return <h1>User: {this.props.match.params.username} </h1>
+        const { user } = this.state
+        return(
+            <div className="github-user">
+                <img src={user.avatar_url} alt='user'/>
+                <h2>{user.login}</h2>
+                <h3>followers: {users.followers}</h3>
+                <h3>following: {users.following}</h3>
+                <h3>location: {users.locatioin}</h3>
+                <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
+                </div>
+        )
     }
 }
 export default GithubUser
