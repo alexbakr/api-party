0dd9053e7fb49e91fba2d437ecc939407fd0b9b0
Alex <bakerab@sbcglobal.net>
2017-05-30 16:27:57 -0400

    Run fetch with new props when location changes

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -15,13 +15,20 @@ class GithubUser extends Component {
 
   constructor(props) {
     super(props)
-    this.fetchUserData()
+    this.fetchUserData(props)
   }
 
-  fetchUserData = () => {
+  fetchUserData = (props) => {
     fetch(`https://api.github.com/users/${this.props.match.params.username}`)
       .then(response => response.json())
       .then(user => this.setState({ user }))
+  }
+
+  componentWillRecieveProps(nextProps) {
+      const locationChanged = nextProps.location !== this.props.location
+      if (locationChanged){
+          this.fetchUserData(nextProps)
+      }
   }
 
   render() {
